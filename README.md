bcf29c499e7158baee8dd4ee52761f46059b446e
Dewanshu Sharma <dewanshu.sharma@kindredgroup.com>
2022-08-04 17:25:27 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NetworkManager
+
+A description of this package.
+
+This package is used for fetching data from server.
---
+++ Sources/NetworkManager/NetworkManager.swift
@@ -0,0 +1,43 @@
+//
+//  FetchItems.swift
+//  Practice
+//
+//  Created by Dewanshu Sharma on 12/7/2022.
+//
+
+import Foundation
+
+protocol HttpProtocol {
+
+    func postRequest<T: Decodable>(with request: URLRequest,
+                        completion: @escaping (Result<T, Error>) -> Void)
+}
+
+class NetworkManager: HttpProtocol {
+
+    func postRequest<T: Decodable>(with request: URLRequest,
+                        completion: @escaping (Result<T, Error>) -> Void) {
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+
+            if let err = error {
+                completion(.failure(err))
+                return
+            }
+
+            if let data = data {
+                print(data)
+                
+                do {
+                    let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decodedResponse))
+                } catch(let error) {
+                    print(error)
+                }
+                
+            }
+        }
+        task.resume()
+      }
+}
+
