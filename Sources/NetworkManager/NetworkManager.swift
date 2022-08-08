3beaed64bb029411cfc30b37b6441e38deac105c
Dewanshu Sharma <dewanshu.sharma@kindredgroup.com>
2022-08-08 22:19:05 +0200

    added post Request function in Url Session extension.

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -20,6 +20,33 @@ extension HttpProtocol {
     }
 }
 
+public extension URLSession {
+    func postRequest<T: Decodable>(with request: URLRequest,
+                        completion: @escaping (Result<T, Error>) -> Void) {
+
+        let task = self.dataTask(with: request) { data, response, error in
+
+            if let err = error {
+                completion(.failure(err))
+                return
+            }
+
+            if let data = data {
+                print(data)
+                
+                do {
+                    let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decodedResponse))
+                } catch(let error) {
+                    print(error)
+                }
+                
+            }
+        }
+        task.resume()
+      }
+}
+
 class NetworkManager: HttpProtocol {
 
     func postRequest<T: Decodable>(with request: URLRequest,
